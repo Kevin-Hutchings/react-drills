d7c5ca441686a5ef9c92eadbf4d74c139d825ace
Kevin-Hutchings <ktmhutchings@gmail.com>
2021-06-17 20:31:33 -0600

    app-6 done

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import ToDo from './ToDo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+  constructor(){
+    super();
+    
+    this.state = {
+      toDoList: [],
+      toDo: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e){
+    this.setState({toDo: e.target.value});
+  }
+
+  handleClick(){
+    let toDoCopy = this.state.toDoList.slice();
+    toDoCopy.push(this.state.toDo);
+    this.setState({toDoList: toDoCopy, toDo: ''});
+  }
+
+  handleReset(){
+    Array.from(document.querySelectorAll('input')).forEach(
+      input => (input.value = '')
+    );
+
+    this.setState({ 
+      toDoList: [],
+      toDo: ''
+    });
+  }
+
+  render(){
+    let listDisplay = this.state.toDoList.map((el, index) => {
+      return <ToDo key={index} task={el}/>
+    });
+
+    return(
+      <div>
+        <h1>My to-do list:</h1>
+        <input value={this.state.toDo} onChange={(e) => this.handleChange(e)}/>
+        <button onClick={this.handleClick}> Add </button>
+        <button className="confirmationButton" onClick={() => this.handleReset()}>clear</button>
+        <ul>
+          {listDisplay}
+        </ul>
+      </div>
+    )
+  }
 }
-
-export default App;
